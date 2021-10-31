eacec83a31ac7e9137197aa5959300c7ad787895
Sardor Shukurov <wiut12467@gmail.com>
2021-10-31 13:06:59 +0500

    Extended logic

--- csf_cw1_12467.py
+++ csf_cw1_12467.py
@@ -13,6 +13,10 @@ def zero_mark():
     print("Your mark is 0")
 
 
+def deferral():
+    print("You get deferral")
+
+
 ans1 = input("Did you upload your file on time? (y/n) \n > ")
 
 
@@ -21,12 +25,25 @@ if ans1 == 'y':
 elif ans1 == 'n':
     ans2 = input("Have you uploaded within 24 hours? (y/n) \n > ")
     if ans2 == 'y':
-        ans3 = input("Is there a valid reason? (y/n) \n > ")
-        if ans3 == 'n':
+        ans2_1 = input("Is there a valid reason? (y/n) \n > ")
+        if ans2_1 == 'n':
             minus_10()
-        elif ans3 == 'y':
-            ans3_1 = input("Have your MC been accepted? (y/n) \n > ")
-            if ans3_1 == 'y':
+        elif ans2_1 == 'y':
+            ans2_2 = input("Has your MC been accepted? (y/n) \n > ")
+            if ans2_2 == 'y':
                 full_mark()
-
+            elif ans2_2 == 'n':
+                minus_10()
+    elif ans2 == 'n':
+        ans3 = input("Have you submitted it within 5 days? (y/n) \n > ")
+        if ans3 == 'n':
+            ans3_1 = input("Is there a valid reason? (y/n) \n > ")
+            if ans3_1 == 'n':
+                zero_mark()
+            elif ans3_1 == 'y':
+                ans3_2 = input("Has your MC before specified deadline been accepted? (y/n) \n > ")
+                if ans3_2 == 'y':
+                    deferral()
+                elif ans3_2 == 'n':
+                    zero_mark()
 
