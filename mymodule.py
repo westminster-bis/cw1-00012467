1547025ee0f7e4ad635872ef2bcb4e87e8c81731
Sardor Shukurov <wiut12467@gmail.com>
2021-11-29 17:53:27 +0500

    Logic is finished. Everything is working

--- mymodule.py
+++ mymodule.py
@@ -37,6 +37,16 @@ def mc_claim2():
         mc_claim2()
 
 
+def mc_claim3():
+    answer = input("Has your MC been accepted? (y/n) \n > ")
+    if answer == 'y':
+        deferral()
+    elif answer == 'n':
+        zero_mark()
+    else:
+        mc_claim3()
+
+
 def within_24():
     print("You have uploaded within 24 hours after the deadline. You got Late Submission")
     answer = input("Is there a valid reason for your Late Submission? (y/n) \n > ")
@@ -58,11 +68,23 @@ def within_5days():
     else:
         within_5days()
 
+
+def out_5days():
+    print("You have not uploaded within 5 days after the deadline.")
+    answer = input("Is there a valid reason for this? (y/n) \n > ")
+    if answer == 'y':
+        mc_claim3()
+    elif answer == 'n':
+        zero_mark()
+    else:
+        out_5days()
+
+
 def upload_date():
-    answer = list(map(int,(input("Input the exact date time when you uploaded your CW (YY/MM/DD/hour/minute) \n > ").split('/'))))
+    answer = list(map(int, (input("Input the exact date and time when you uploaded the CW(YY/MM/DD/hour/minute) \n > ").split('/'))))
     uploaddate = datetime.datetime(answer[0], answer[1], answer[2], answer[3], answer[4])
 
-    answer = list(map(int,(input("Input the exact date and time when the deadline for your CW was (YY/MM/DD/hour/minute) \n > ").split('/'))))
+    answer = list(map(int, (input("Input the exact date and time of deadline for your CW (YY/MM/DD/hour/minute) \n > ").split('/'))))
     deadline = datetime.datetime(answer[0], answer[1], answer[2], answer[3], answer[4])
 
     difference = deadline - uploaddate
@@ -76,7 +98,6 @@ def upload_date():
     day_dif = date1 - date2
     day_dif2 = date3 - date4
 
-
     if deadline < uploaddate:
         if difference >= day_dif:
             within_24()
@@ -84,8 +105,10 @@ def upload_date():
             if day_dif > difference >= day_dif2:
                 within_5days()
             else:
-
+                out_5days()
     elif deadline >= uploaddate:
         full_mark()
+
+
 if __name__ == '__main__':
     upload_date()
