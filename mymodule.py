87e977258e514b9dcce287494f0899a84c445ab6
Sardor Shukurov <wiut12467@gmail.com>
2021-11-29 15:27:15 +0500

    Found out how to check if the difference between deadline and uploaded date is between 24 hours or more

--- mymodule.py
+++ mymodule.py
@@ -9,5 +9,17 @@ def upload_date():
     thedate = list(map(int, answer.split('/')))
     deadline = datetime.datetime(thedate[0], thedate[1], thedate[2], thedate[3], thedate[4])
 
+    difference = deadline - uploaddate
+    date1 = datetime.datetime(2021,11,26,23,59)
+    date2 = datetime.datetime(2021, 11, 27, 23, 59)
+    day_dif = date1 - date2
+    if deadline < uploaddate:
+        if difference >= day_dif:
+            print("Difference is between 24 hours")
+        elif difference < day_dif:
+            print("Difference is not between 24 hours")
+    elif deadline >= uploaddate:
+        print("W")
+
 if __name__ == '__main__':
     upload_date()
